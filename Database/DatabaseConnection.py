62deacada4b0d8f85062596aec901d0b46f66e32
aniaszpala14 <s27542@pjwstk.edu.pl>
2024-06-03 21:05:52 +0200

    zaprawiłam kolejność w liście

--- Database/DatabaseConnection.py
+++ Database/DatabaseConnection.py
@@ -1,8 +1,6 @@
 import sqlite3
 
 from Models.Symptoms import Symptoms
-
-
 class Connection():
     con: sqlite3.Connection
 
@@ -18,10 +16,12 @@ class Connection():
         for przypadek in przypadki:
             cur.execute("""SELECT * FROM Choroba WHERE idCh = przypadek[idCh]""")
             choroba = cur.fetchall()
-            list.append(Symptoms(przypadek['apatia'], przypadek['bol_w_klatce'], przypadek['goraczka'],
-                                 przypadek['kaszel'], przypadek['flegma'], przypadek['krwioplucie'],
-                                 przypadek['nocne_poty'], przypadek['obrzek_nog'], przypadek['sennosc'],
-                                 przypadek['sinica'], przypadek['splycenie_oddechu'], przypadek['suchosc_w_ustach'],
-                                 przypadek['swiszczacy_oddech'], przypadek['szybkie_bicie_serca'],
-                                 przypadek['utrudnione_oddychanie'], przypadek['zawroty_glowy'], przypadek['zmeczenie'],
-                                 przypadek['zmniejszony'], choroba[]))
+            list.append(Symptoms(przypadek['kaszel'],przypadek['dusznosc'],przypadek['bol_w_klatce'],
+                                 przypadek['goraczka'],przypadek['swiszczacy_oddech'],przypadek['utrata_wagi'],
+                                 przypadek['zmeczenie'],przypadek['krwioplucie'],przypadek['nocne_poty'],
+                                 przypadek['splycenie_oddechu'],przypadek['sinica'],przypadek['apatia'],przypadek['flegma'],
+                                 przypadek['obrzek_nog'],przypadek['suchosc_w_ustach'],przypadek['zmniejszony_apetyt'],
+                                 przypadek['utrudnione_oddychanie'],przypadek['szybkie_bicie_serca'],
+                                 przypadek['sennosc'],przypadek['zawroty_glowy'],choroba['nazwa']))
+
+
