cb38580ba62381c4dbf7a4069926e12ba8dfbec9
aniaszpala14 <s27542@pjwstk.edu.pl>
2024-06-03 21:32:23 +0200

    dodalam zczytywanie chorob do listy i metode ktora tworzy mape K:idCh V:lista przypadkow do niej

--- Database/DatabaseConnection.py
+++ Database/DatabaseConnection.py
@@ -6,6 +6,7 @@ class Connection():
     con: sqlite3.Connection
     list: []
     list_choroby: []
+    map_poidCh: {}
 
     def __init__(self):
         self.con = sqlite3.connect('master')
@@ -17,15 +18,15 @@ class Connection():
         przypadki = self.cur.fetchall()
         list = []
         for przypadek in przypadki:
-            cur.execute("""SELECT * FROM Choroba WHERE idCh = przypadek[idCh]""")
-            choroba = cur.fetchall()
+          # cur.execute("""SELECT * FROM Choroba WHERE idCh = przypadek[idCh]""")
+          #  choroba = cur.fetchall()
             list.append(Symptoms(przypadek['kaszel'],przypadek['dusznosc'],przypadek['bol_w_klatce'],
                                  przypadek['goraczka'],przypadek['swiszczacy_oddech'],przypadek['utrata_wagi'],
                                  przypadek['zmeczenie'],przypadek['krwioplucie'],przypadek['nocne_poty'],
                                  przypadek['splycenie_oddechu'],przypadek['sinica'],przypadek['apatia'],przypadek['flegma'],
                                  przypadek['obrzek_nog'],przypadek['suchosc_w_ustach'],przypadek['zmniejszony_apetyt'],
                                  przypadek['utrudnione_oddychanie'],przypadek['szybkie_bicie_serca'],
-                                 przypadek['sennosc'],przypadek['zawroty_glowy'],choroba['idCh']))
+                                 przypadek['sennosc'],przypadek['zawroty_glowy'],przypadek['idCh']))
 
     def chorobyToList(self):
         self.con.row_factory = sqlite3.Row
@@ -36,6 +37,14 @@ class Connection():
         for choroba in choroby:
             list_choroby.append(ChorobaModel(choroba['idCh'],choroba['nazwa']))
         return list_choroby
+
+    def podzielListPoidCh(self):
+        list = self.przypadkiToList()
+        list_choroby = self.chorobyToList()
+        map_poidCh = {}
+        for choroba in list_choroby:
+            map_poidCh[choroba.idCh] = [x for x in list if x.choroba == choroba.idCh]
+        return map_poidCh
 
 
 
