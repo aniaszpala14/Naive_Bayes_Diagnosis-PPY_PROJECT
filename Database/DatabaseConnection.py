29a782350eec5892e91c77a2194dc9ad9dd24930
aniaszpala14 <s27542@pjwstk.edu.pl>
2024-06-07 19:06:55 +0200

    dodalam laczeni z baza mssql i ta komunikacje flask html

--- Database/DatabaseConnection.py
+++ Database/DatabaseConnection.py
@@ -1,53 +1,55 @@
-import sqlite3
-
-from Models.Disease import  Disease
+import pyodbc
+from Models.Disease import Disease
 from Models.Symptoms import Symptoms
-class DatabaseConnection():
-    con: sqlite3.Connection
-    list: []
-    list_choroby: []
-    map_poidCh: {}
 
+class DatabaseConnection:
     def __init__(self):
-        self.con = sqlite3.connect('master')
+        self.con = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};'
+            'SERVER=localhost;'
+            'PORT=1433;'
+            'DATABASE=master;'
+            'UID=sa;'
+            'PWD=ania1410@DOCKER'
+        )
+        self.list = []
+        self.list_choroby = []
+        self.map_poidCh = {}
+        print("Connected!")
+        self.podzielListPoidCh()
 
-    def przypadkiToList(self) -> []:
-        self.con.row_factory = sqlite3.Row
+    def przypadkiToList(self) -> list:
         cur = self.con.cursor()
         cur.execute("""SELECT * FROM Przypadek""")
-        przypadki = self.cur.fetchall()
+        przypadki = cur.fetchall()
         list = []
         for przypadek in przypadki:
-          # cur.execute("""SELECT * FROM Choroba WHERE idCh = przypadek[idCh]""")
-          #  choroba = cur.fetchall()
-            list.append(Symptoms(przypadek['kaszel'],przypadek['dusznosc'],przypadek['bol_w_klatce'],
-                                 przypadek['goraczka'],przypadek['swiszczacy_oddech'],przypadek['utrata_wagi'],
-                                 przypadek['zmeczenie'],przypadek['krwioplucie'],przypadek['nocne_poty'],
-                                 przypadek['splycenie_oddechu'],przypadek['sinica'],przypadek['apatia'],przypadek['flegma'],
-                                 przypadek['obrzek_nog'],przypadek['suchosc_w_ustach'],przypadek['zmniejszony_apetyt'],
-                                 przypadek['utrudnione_oddychanie'],przypadek['szybkie_bicie_serca'],
-                                 przypadek['sennosc'],przypadek['zawroty_glowy'],przypadek['idCh']))
+            list.append(Symptoms(
+                przypadek.kaszel, przypadek.dusznosc, przypadek.bol_w_klatce,
+                przypadek.goraczka, przypadek.swiszczacy_oddech, przypadek.utrata_wagi,
+                przypadek.zmeczenie, przypadek.krwioplucie, przypadek.nocne_poty,
+                przypadek.splycenie_oddechu, przypadek.sinica, przypadek.apatia,
+                przypadek.flegma, przypadek.obrzek_nog, przypadek.suchosc_w_ustach,
+                przypadek.zmniejszony_apetyt, przypadek.utrudnione_oddyhanie,
+                przypadek.szybkie_bicie_serca, przypadek.sennosc, przypadek.zawroty_glowy,
+                przypadek.idCh
+            ))
+        return list
 
-    def chorobyToList(self) -> []:
-        self.con.row_factory = sqlite3.Row
+    def chorobyToList(self) -> list:
         cur = self.con.cursor()
         cur.execute("""SELECT * FROM Choroba""")
         choroby = cur.fetchall()
         list_choroby = []
         for choroba in choroby:
-            list_choroby.append(Disease(choroba['idCh'],choroba['nazwa']))
+            list_choroby.append(Disease(choroba.idCh, choroba.nazwa))
         return list_choroby
 
-    def podzielListPoidCh(self) -> {}:
-        list = self.przypadkiToList()
+    def podzielListPoidCh(self) -> dict:
+        list_przypadkow = self.przypadkiToList()
         list_choroby = self.chorobyToList()
         map_poidCh = {}
         for choroba in list_choroby:
-            map_poidCh[choroba.idCh] = [x for x in list if x.choroba == choroba.idCh]
+            map_poidCh[choroba.idCh] = [x for x in list_przypadkow if x.choroba == choroba.idCh]
+        self.map_poidCh = map_poidCh
         return map_poidCh
-
-
-
-
-
-
---
+++ app2.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, render_template
+
+from Classifiers.Classifire import Classifire
+from Database.DatabaseConnection import DatabaseConnection
+from Models.Symptoms import Symptoms
+
+app2 = Flask(__name__)
+
+
+@app2.route('/')
+def index():
+    return render_template('index.html')
+
+
+def validate_symptom(value):
+    pass
+
+
+@app2.route('/submit', methods=['GET'])
+def submit():
+    apatia = request.args.get('apatia')
+    bol_w_klatce = request.args.get('bol_w_klatce')
+    goraczka = request.args.get('goraczka')
+    kaszel = request.args.get('kaszel')
+    flegma = request.args.get('kaszel_z_flegma')
+    krwioplucie = request.args.get('krwioplucie')
+    nocne_poty = request.args.get('nocne_poty')
+    obrzek_nog = request.args.get('obrzek_nog')
+    sennosc = request.args.get('sennosc')
+    sinica = request.args.get('sinica')
+    splycenie_oddechu = request.args.get('splycenie_oddechu')
+    suchosc_w_ustach = request.args.get('suchosc_w_ustach')
+    swiszczacy_oddech = request.args.get('swiszczacy_oddech')
+    szybkie_bicie_serca = request.args.get('szybkie_bicie_serca')
+    utrata_wagi = request.args.get('utrata_wagi')
+    utrudnione_oddychanie = request.args.get('utrudnione_oddychanie')
+    zawroty_glowy = request.args.get('zawroty_glowy')
+    zmeczenie = request.args.get('zmeczenie')
+    zmniejszony_apetyt = request.args.get('zmniejszony_apetyt')
+    dusznosc = request.args.get('dusznosc')
+
+    for value in apatia, bol_w_klatce, goraczka, kaszel, flegma, krwioplucie, nocne_poty, obrzek_nog, sennosc, sinica, splycenie_oddechu, suchosc_w_ustach, swiszczacy_oddech, szybkie_bicie_serca, utrata_wagi, utrudnione_oddychanie, zawroty_glowy, zmeczenie, zmniejszony_apetyt:
+        if not validate_symptom(value):
+            return ({"error": f"Invalid value for {value}"}), 400
+
+    symptoms = Symptoms(apatia, bol_w_klatce, goraczka, kaszel, flegma, krwioplucie, nocne_poty, obrzek_nog,
+                            sennosc,
+                            sinica, splycenie_oddechu, suchosc_w_ustach, swiszczacy_oddech, szybkie_bicie_serca,
+                            utrata_wagi, utrudnione_oddychanie, zawroty_glowy, zmeczenie, zmniejszony_apetyt, dusznosc,
+                            "")
+
+    base = DatabaseConnection()
+    mapa = base.map_poidCh
+
+    if mapa.items()==None:
+        print("mapa jest pusta")
+    for key, value in mapa.items():
+        print(f"{key}: {value}")
+        print("lala")
+
+    classifire = Classifire(symptoms, mapa)
+
+
+    wynik = "jeszcze nic "
+    return render_template('result.html', wynik=wynik)
+
+
+def validate_symptom(symptom):
+    valid_values = {"tak", "nie", "czasami"}
+    if symptom not in valid_values:
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    app2.run(debug=True)
